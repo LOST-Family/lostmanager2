a1615212e87c47ab089938403223730f97ac9cdd
uniquepixel <joni.dormann@gmail.com>
2025-12-15 23:44:26 +0100

    formalities

--- src/main/java/lostmanager/Bot.java
+++ src/main/java/lostmanager/Bot.java
@@ -1,12 +1,10 @@
 package lostmanager;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
